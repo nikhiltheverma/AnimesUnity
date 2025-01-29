f0fe32e4b1be8080fc204dd9634d8ff88d7ddce8
Nikhil Verma <139608274+nikhiltheverma@users.noreply.github.com>
2025-01-29 10:56:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,18 +3,6 @@
 const IndexApi = "https://api3.nikhilvermaultimate.workers.dev/home";
 const recentapi = "https://api3.nikhilvermaultimate.workers.dev/recent/";
 
-const { NekosiaAPI } = require('nekosia.js');
-
-(async () => {
-	const response1 = await NekosiaAPI.fetchCategoryImages('catgirl');
-	console.log(response1);
-	
-	const response2 = await NekosiaAPI.fetchImages({ session: 'ip', count: 1, tags: ['cute', 'blue-hair'], blacklist: ['yellow-hair'] });
-	console.log(response2);
-	
-	// https://nekosia.cat/documentation?page=api-endpoints#example-response
-})();
-
 // Usefull functions
 
 async function getJson(url) {
