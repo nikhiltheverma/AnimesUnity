c50e37933972ed4f859b061b382008c1ca207919
Nikhil Verma <139608274+nikhiltheverma@users.noreply.github.com>
2025-07-06 17:03:41 +0530

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -1,12 +1,7 @@
-// const animeapi = "https://api3.nikhilvermaultimate.workers.dev/anime/";
-// const episodeapi = "https://api3.nikhilvermaultimate.workers.dev/episode/";
-// const dlapi = "https://api3.nikhilvermaultimate.workers.dev/download/";
-
 const animeapi = "https://animeunity.vercel.app/anime/zoro/info?id=";
 const episodeapi = "https://animeunity.vercel.app/anime/zoro/watch?episodeId=";
 
-// Usefull functions
-
+// Useful functions
 async function getJson(url) {
     try {
         const response = await fetch(url);
@@ -16,32 +11,33 @@ async function getJson(url) {
     }
 }
 
-function sentenceCase(str) {
-    if (str === null || str === "") return false;
-    else str = str.toString();
-
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// Function to get m3u8 url of episode
+// Load video into iframe
 async function loadVideo(name, sources) {
     try {
         document.getElementById("ep-name").innerHTML = name;
         const serversbtn = document.getElementById("serversbtn");
 
+        if (!sources || sources.length === 0) {
+            serversbtn.innerHTML = `<div class="sitem error-text">No servers available for this episode.</div>`;
+            document.getElementById("AnimeDexFrame").style.display = "none";
+            return false;
+        }
+
         let url = sources[0]["url"];
-        serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn sactive" onclick="selectServer(this)" data-value="./embed.html?url=${url}">Zoro Server 1</a> </div>`;
+        serversbtn.innerHTML += `<div class="sitem"> 
+            <a class="sobtn sactive" onclick="selectServer(this)" data-value="./embed.html?url=${url}">Zoro Server 1</a> 
+        </div>`;
         document.getElementsByClassName("sactive")[0].click();
 
         if (sources.length > 1) {
             let url2 = sources[1]["url"];
-            serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn" onclick="selectServer(this)" data-value="./embed.html?url=${url2}">Zoro Server 2</a> </div>`;
+            serversbtn.innerHTML += `<div class="sitem"> 
+                <a class="sobtn" onclick="selectServer(this)" data-value="./embed.html?url=${url2}">Zoro Server 2</a> 
+            </div>`;
         }
 
         return true;
@@ -51,18 +47,19 @@ async function loadVideo(name, sources) {
     }
 }
 
-// Function to select server
+// Select streaming server
 function selectServer(btn) {
     const buttons = document.getElementsByClassName("sobtn");
     const iframe = document.getElementById("AnimeDexFrame");
     iframe.src = btn.getAttribute("data-value");
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].className = "sobtn";
     }
     btn.className = "sobtn sactive";
 }
 
-// Function to get episode list
+// Load episode list
 async function getEpList(anime_id) {
     const data = await getJson(animeapi + anime_id);
     const eplist = data["episodes"];
@@ -77,46 +74,42 @@ async function getEpList(anime_id) {
     return eplist;
 }
 
-// Function to get selector btn
+// Previous/Next episode buttons
 async function getSelectorBtn(eplist, currentId) {
-    let currentIndex = eplist.findIndex(ep => ep.id === currentId);
-
+    let currentIndex = eplist.findIndex(ep => ep.id.toString() === currentId);
     let html = "";
-    if (eplist.length < 2) {
-        html = "";
-    } else {
-        if (currentIndex > 0) {
-            html += `<a class="btns" href="./episode.html?anime=${urlParams.get("anime")}&episode=${eplist[currentIndex - 1].id}">
-                <button class="sbtn inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    <i class="fa fa-arrow-circle-left"></i> Episode ${eplist[currentIndex - 1].number}
-                </button></a>`;
-        }
 
-        if (currentIndex < eplist.length - 1) {
-            html += `<a class="btns" href="./episode.html?anime=${urlParams.get("anime")}&episode=${eplist[currentIndex + 1].id}">
-                <button class="sbtn inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Episode ${eplist[currentIndex + 1].number} <i style="margin-left:10px" class="fa fa-arrow-circle-right"></i>
-                </button></a>`;
-        }
+    if (currentIndex > 0) {
+        html += `<a class="btns" href="./episode.html?anime=${urlParams.get("anime")}&episode=${eplist[currentIndex - 1].id}">
+            <button class="sbtn inline-flex text-white bg-indigo-500 border-0 py-2 px-6 hover:bg-indigo-600 rounded text-lg">
+                <i class="fa fa-arrow-circle-left"></i> Episode ${eplist[currentIndex - 1].number}
+            </button></a>`;
+    }
+
+    if (currentIndex < eplist.length - 1) {
+        html += `<a class="btns" href="./episode.html?anime=${urlParams.get("anime")}&episode=${eplist[currentIndex + 1].id}">
+            <button class="sbtn inline-flex text-white bg-indigo-500 border-0 py-2 px-6 hover:bg-indigo-600 rounded text-lg">
+                Episode ${eplist[currentIndex + 1].number} <i class="fa fa-arrow-circle-right" style="margin-left:10px"></i>
+            </button></a>`;
     }
 
     document.getElementsByClassName("selector")[0].innerHTML = html;
 }
 
+// Get URL parameters
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-if (urlParams.get("anime") == null || urlParams.get("episode") == null) {
+if (!urlParams.get("anime") || !urlParams.get("episode")) {
     window.location = "./index.html";
 }
 
-// Running functions
+// Main execution
 getJson(episodeapi + urlParams.get("episode")).then((data) => {
     const sources = data["sources"];
     const epTitle = data["title"];
 
-    document.documentElement.innerHTML =
-        document.documentElement.innerHTML.replaceAll("{{ title }}", epTitle);
+    document.documentElement.innerHTML = document.documentElement.innerHTML.replaceAll("{{ title }}", epTitle);
 
     loadVideo(epTitle, sources).then(() => {
         console.log("Video loaded");
