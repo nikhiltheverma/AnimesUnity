550699438c790a91a1e8167ef22c8f63f0755b4c
Nikhil Verma <139608274+nikhiltheverma@users.noreply.github.com>
2025-07-06 13:24:42 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 // const searchapi = "https://api3.nikhilvermaultimate.workers.dev/search/";
-const searchapi = "https://animedex.in/src/assets/js/search.js?v=1.2";
+// const searchapi = "https://animedex.in/src/assets/js/search.js?v=1.2";
 const searchapi = "https://animeunity.vercel.app/anime/zoro/search?query=";
 // Usefull functions
 
